28daff224a6095e231153940fc78a136ae21c54f
Daniel Jones <104509116+DanielReddJones@users.noreply.github.com>
2022-11-10 11:39:43 -0600

    File Dialog
    Added file dialog function

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,5 +1,10 @@
 from pytube import *
 from tkinter import *
+from tkinter import filedialog
+
+
+root = Tk()
+root.title("video download screen")
 
 def main():
 
@@ -9,20 +14,26 @@ def main():
 
 #Tkinter screen setup
 def main_screen():
-    placeholder_text = "filepath"
     
-    root = Tk()
-    root.title("video download screen")
+    
+
     url_label = Label(root, text = "URL: ").grid(column = 0, row = 0)
     text_box = Text(root, height = 1, width = 30).grid(column = 1, row = 0)
+
     save_label = Label(root, text = "save to: ").grid(column = 0, row = 1)
-    save_button = Button(root, text = "...").grid(column = 2, row = 1)
-    save_text = Label(root, text = placeholder_text).grid(column = 1, row = 1)
+    save_button = Button(root, text = "...", command = save_location).grid(column = 2, row = 1)
+    save_text = Label(root, text = "c:\\").grid(column = 1, row = 1)
+
     download_button = Button(root, text = "download").grid(column = 1, row = 2)
 
-    placeholder_text = "changed"    
+def save_location():
+    filepath = filedialog.askdirectory(initialdir=r"C:\Users")
+    save_text = Label(root, text = filepath).grid(column = 1, row = 1)
+    return filepath
     
     
 
+def download_vid():
+    print("placeholder for pytube code")
 
 main()
