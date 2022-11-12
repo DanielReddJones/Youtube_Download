ab53954958179b2bd25a5a94f22af6f3cc140b85
Daniel Jones <104509116+DanielReddJones@users.noreply.github.com>
2022-11-12 13:28:29 -0600

    Update youtube_downloader.py
    Made URL entry box accessible to download method. TODO: have grid object pass text to variable

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -4,7 +4,8 @@ from tkinter import filedialog
 
 root = Tk()
 root.title("video download screen")
-
+url_label = Label(root, text="URL: ").grid(column=0, row=0)
+text_box = Text(root, height=1, width=30).grid(column=1, row=0)
 
 def main():
     main_screen()
@@ -12,8 +13,6 @@ def main():
 
 # Tkinter screen setup
 def main_screen():
-    url_label = Label(root, text="URL: ").grid(column=0, row=0)
-    text_box = Text(root, height=1, width=30).grid(column=1, row=0)
 
     save_label = Label(root, text="save to: ").grid(column=0, row=1)
     save_button = Button(root, text="...", command=save_location).grid(column=2, row=1)
@@ -25,21 +24,23 @@ def main_screen():
 # grabs filepath and updates the label to display the new filepath.
 def save_location():
     global filepath
+    #if ran on Linux/MacOS, will default to the original folder file is in. Otherwise will open in C:\
     filepath = filedialog.askdirectory(initialdir=r"C:\Users")
     save_text = Label(root, text=filepath).grid(column=1, row=1)
-
+    
 
 # placeholder for now. Will update later to download the videos to the filepath.
 def download_vid():
-    try:
-        yt = YouTube(text_box.get('1.0', 'end'))
-        mp3Files = yt.filter('mp3')
-        my_vid = youtube.streams.first()
-        my_vid.download(filepath)
+    #TODO: re-add try/except when downloading works.
+    #try:
+    yt = YouTube(text_box.get('1.0', 'end'))
+    mp3Files = yt.filter('mp3')
+    my_vid = youtube.streams.first()
+    my_vid.download(filepath)
 
 
-    except NameError:
-        print("select a filepath, and insert a youtube URL in the URL box.")
+    #except NameError:
+        #print("select a filepath, and insert a youtube URL in the URL box.")
 
 
 if __name__ == "__main__":
